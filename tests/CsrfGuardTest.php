8e01dae6072b16f1a1673cb3f2b9740f5fd5573c
s3b4stian <sebastian.rapetti@tim.it>
2019-04-20 11:18:49 +0200

    php7.3 compatibility fixes

--- tests/CsrfGuardTest.php
+++ tests/CsrfGuardTest.php
@@ -71,7 +71,6 @@ class CsrfGuardTest extends TestCase
      * @dataProvider contructorWrongArgumentsProvider
      *
      * @expectedException TypeError
-     * @expectedExceptionMessageRegExp /Argument 1 passed to Linna\\CsrfGuard::__construct\(\) must be of the type integer, (string|boolean|float|object|array) given/
      */
     public function testNewInstanceWithWrongArguments($maxStorage, $tokenStrength): void
     {
@@ -357,7 +356,6 @@ class CsrfGuardTest extends TestCase
      * Test Garbage Collector with wrong arguments.
      *
      * @expectedException TypeError
-     * @expectedExceptionMessageRegExp /Argument 1 passed to Linna\\CsrfGuard::garbageCollector\(\) must be of the type integer, boolean given/
      *
      * @runInSeparateProcess
      */
